3c43f044480825fda4e04e2ae657343615686029
Alexander Ko <ako7@myseneca.ca>
2024-11-17 23:52:47 -0500

    Lab6 completed ako7

--- lab6a.py
+++ lab6a.py
@@ -35,24 +35,24 @@ class Student:
 if __name__ == '__main__':
 
  #This is going to be object for Student john as student 1
- student1 = Student('John', '013454900')
+ student1 = Student('John', '543210')
  student1.addGrade('uli101', 1.0)
- student1.addGrade('ops245', 2.0)
- student1.addGrade('ops445', 3.0)
+ student1.addGrade('ops535', 3.0)
+ student1.addGrade('ops445', 4.0)
 
 
- #This is going to be object 2 for student Jessica as student 2
- student2 = Student('Jessica', 123456)
- student2.addGrade('ipc144', 4.0)
- student2.addGrade('cpp244', 3.5)
- student2.addGrade('cpp344', 0.0)
+ #This is going to be object 2 for student Alexander as student 2
+ student2 = Student('Alexander', 123456)
+ student2.addGrade('ipc144', 3.0)
+ student2.addGrade('ops200', 2.5)
+ student2.addGrade('ops300', 0.0)
 
 
  print(student1.displayStudent()) #this isdisplaying student 1 John
  print(student1.displayGPA())
  print(student1.displayCourses())
 
- print(student2.displayStudent()) #this is displaying student 2 jessica
+ print(student2.displayStudent()) #this is displaying student 2 Alexander
  print(student2.displayGPA())
  print(student2.displayCourses())
 
